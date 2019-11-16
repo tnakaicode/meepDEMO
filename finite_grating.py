02c8a432b2a90d7e3a703ca7156518a3114a3c21
Taku Nakai <tnakaicode@gmail.com>
2019-11-17 01:51:07 +0900

    run ring

--- finite_grating.py
+++ finite_grating.py
@@ -21,7 +21,7 @@ num_cells = 5           # number of grating unit cells
 dair = 10 if field_profile else dpad
 
 wvl = 0.5               # center wavelength
-fcen = 1/wvl            # center frequency
+fcen = 1 / wvl            # center frequency
 
 k_point = mp.Vector3()
 
@@ -29,21 +29,21 @@ glass = mp.Medium(index=1.5)
 
 pml_layers = [mp.PML(thickness=dpml)]
 
-symmetries=[mp.Mirror(mp.Y)]
+symmetries = [mp.Mirror(mp.Y)]
 
-sx = dpml+dsub+gh+dair+dpml
-sy = dpml+dpad+num_cells*gp+dpad+dpml
-cell_size = mp.Vector3(sx,sy)
+sx = dpml + dsub + gh + dair + dpml
+sy = dpml + dpad + num_cells * gp + dpad + dpml
+cell_size = mp.Vector3(sx, sy)
 
-src_pt = mp.Vector3(-0.5*sx+dpml+0.5*dsub)
-sources = [mp.Source(mp.GaussianSource(fcen,fwidth=0.2*fcen),
+src_pt = mp.Vector3(-0.5 * sx + dpml + 0.5 * dsub)
+sources = [mp.Source(mp.GaussianSource(fcen, fwidth=0.2 * fcen),
                      component=mp.Ez,
                      center=src_pt,
-                     size=mp.Vector3(y=sy-2*dpml))]
+                     size=mp.Vector3(y=sy - 2 * dpml))]
 
 geometry = [mp.Block(material=glass,
-                     size=mp.Vector3(dpml+dsub,mp.inf,mp.inf),
-                     center=mp.Vector3(-0.5*sx+0.5*(dpml+dsub)))]
+                     size=mp.Vector3(dpml + dsub, mp.inf, mp.inf),
+                     center=mp.Vector3(-0.5 * sx + 0.5 * (dpml + dsub)))]
 
 sim = mp.Simulation(resolution=resolution,
                     cell_size=cell_size,
@@ -53,8 +53,9 @@ sim = mp.Simulation(resolution=resolution,
                     sources=sources,
                     symmetries=symmetries)
 
-mon_pt = mp.Vector3(0.5*sx-dpml-0.5*dair)
-near_fields = sim.add_dft_fields([mp.Ez], fcen, fcen, 1, center=mon_pt, size=mp.Vector3(dair if field_profile else 0,sy-2*dpml))
+mon_pt = mp.Vector3(0.5 * sx - dpml - 0.5 * dair)
+near_fields = sim.add_dft_fields([mp.Ez], fcen, fcen, 1, center=mon_pt, size=mp.Vector3(
+    dair if field_profile else 0, sy - 2 * dpml))
 
 sim.run(until_after_sources=100)
 
@@ -63,9 +64,9 @@ flat_dft = sim.get_dft_array(near_fields, mp.Ez, 0)
 sim.reset_meep()
 
 for j in range(num_cells):
-  geometry.append(mp.Block(material=glass,
-                           size=mp.Vector3(gh,gdc*gp,mp.inf),
-                           center=mp.Vector3(-0.5*sx+dpml+dsub+0.5*gh,-0.5*sy+dpml+dpad+(j+0.5)*gp)))
+    geometry.append(mp.Block(material=glass,
+                             size=mp.Vector3(gh, gdc * gp, mp.inf),
+                             center=mp.Vector3(-0.5 * sx + dpml + dsub + 0.5 * gh, -0.5 * sy + dpml + dpad + (j + 0.5) * gp)))
 
 sim = mp.Simulation(resolution=resolution,
                     cell_size=cell_size,
@@ -75,51 +76,53 @@ sim = mp.Simulation(resolution=resolution,
                     sources=sources,
                     symmetries=symmetries)
 
-near_fields = sim.add_dft_fields([mp.Ez], fcen, fcen, 1, center=mon_pt, size=mp.Vector3(dair if field_profile else 0,sy-2*dpml))
+near_fields = sim.add_dft_fields([mp.Ez], fcen, fcen, 1, center=mon_pt, size=mp.Vector3(
+    dair if field_profile else 0, sy - 2 * dpml))
 
 sim.run(until_after_sources=100)
 
 grating_dft = sim.get_dft_array(near_fields, mp.Ez, 0)
 
-scattered_field = grating_dft-flat_dft
+scattered_field = grating_dft - flat_dft
 scattered_amplitude = np.abs(scattered_field)**2
 
-[x,y,z,w] = sim.get_array_metadata(dft_cell=near_fields)
+[x, y, z, w] = sim.get_array_metadata(dft_cell=near_fields)
 
 if field_profile:
-  if mp.am_master():
-    plt.figure(dpi=150)
-    plt.pcolormesh(x,y,np.rot90(scattered_amplitude),cmap='inferno',shading='gouraud',vmin=0,vmax=scattered_amplitude.max())
-    plt.gca().set_aspect('equal')
-    plt.xlabel('x (μm)')
-    plt.ylabel('y (μm)')
+    if mp.am_master():
+        plt.figure(dpi=150)
+        plt.pcolormesh(x, y, np.rot90(scattered_amplitude), cmap='inferno',
+                       shading='gouraud', vmin=0, vmax=scattered_amplitude.max())
+        plt.gca().set_aspect('equal')
+        plt.xlabel('x (μm)')
+        plt.ylabel('y (μm)')
 
-    # ensure that the height of the colobar matches that of the plot
-    from mpl_toolkits.axes_grid1 import make_axes_locatable
-    divider = make_axes_locatable(plt.gca())
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(cax=cax)
-    plt.tight_layout()
-    plt.show()
+        # ensure that the height of the colobar matches that of the plot
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+        divider = make_axes_locatable(plt.gca())
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(cax=cax)
+        plt.tight_layout()
+        plt.show()
 else:
-  ky = np.fft.fftshift(np.fft.fftfreq(len(scattered_field), 1/resolution))
-  FT_scattered_field = np.fft.fftshift(np.fft.fft(scattered_field))
-  if mp.am_master():
-    plt.figure(dpi=150)
-    plt.subplots_adjust(hspace=0.3)
+    ky = np.fft.fftshift(np.fft.fftfreq(len(scattered_field), 1 / resolution))
+    FT_scattered_field = np.fft.fftshift(np.fft.fft(scattered_field))
+    if mp.am_master():
+        plt.figure(dpi=150)
+        plt.subplots_adjust(hspace=0.3)
 
-    plt.subplot(2,1,1)
-    plt.plot(y,scattered_amplitude,'bo-')
-    plt.gca().get_yaxis().set_ticks([])
-    plt.xlabel("y (μm)")
-    plt.ylabel("field amplitude (a.u.)")
+        plt.subplot(2, 1, 1)
+        plt.plot(y, scattered_amplitude, 'bo-')
+        plt.gca().get_yaxis().set_ticks([])
+        plt.xlabel("y (μm)")
+        plt.ylabel("field amplitude (a.u.)")
 
-    plt.subplot(2,1,2)
-    plt.plot(ky,np.abs(FT_scattered_field)**2,'ro-')
-    plt.gca().get_yaxis().set_ticks([])
-    plt.xlabel(r'wavevector k$_y$, 2π (μm)$^{-1}$')
-    plt.ylabel("Fourier transform (a.u.)")
-    plt.gca().set_xlim([-3, 3])
+        plt.subplot(2, 1, 2)
+        plt.plot(ky, np.abs(FT_scattered_field)**2, 'ro-')
+        plt.gca().get_yaxis().set_ticks([])
+        plt.xlabel(r'wavevector k$_y$, 2π (μm)$^{-1}$')
+        plt.ylabel("Fourier transform (a.u.)")
+        plt.gca().set_xlim([-3, 3])
 
-    plt.tight_layout(pad=1.0)
-    plt.show()
+        plt.tight_layout(pad=1.0)
+        plt.show()
--- ring.py
+++ ring.py
@@ -3,19 +3,20 @@ from __future__ import division
 
 import meep as mp
 
+
 def main():
     n = 3.4                 # index of waveguide
     w = 1                   # width of waveguide
     r = 1                   # inner radius of ring
     pad = 4                 # padding between waveguide and edge of PML
     dpml = 2                # thickness of PML
-    sxy = 2*(r+w+pad+dpml)  # cell size
+    sxy = 2 * (r + w + pad + dpml)  # cell size
 
     # Create a ring waveguide by two overlapping cylinders - later objects
     # take precedence over earlier objects, so we put the outer cylinder first.
     # and the inner (air) cylinder second.
 
-    c1 = mp.Cylinder(radius=r+w, material=mp.Medium(index=n))
+    c1 = mp.Cylinder(radius=r + w, material=mp.Medium(index=n))
     c2 = mp.Cylinder(radius=r)
 
     # If we don't want to excite a specific mode symmetry, we can just
@@ -25,7 +26,8 @@ def main():
     fcen = 0.15             # pulse center frequency
     df = 0.1                # pulse width (in frequency)
 
-    src = mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))
+    src = mp.Source(mp.GaussianSource(fcen, fwidth=df),
+                    mp.Ez, mp.Vector3(r + 0.1))
 
     sim = mp.Simulation(cell_size=mp.Vector3(sxy, sxy),
                         geometry=[c1, c2],
@@ -35,13 +37,14 @@ def main():
                         boundary_layers=[mp.PML(dpml)])
 
     sim.run(mp.at_beginning(mp.output_epsilon),
-            mp.after_sources(mp.Harminv(mp.Ez, mp.Vector3(r+0.1), fcen, df)),
+            mp.after_sources(mp.Harminv(mp.Ez, mp.Vector3(r + 0.1), fcen, df)),
             until_after_sources=300)
 
     # Output fields for one period at the end.  (If we output
     # at a single time, we might accidentally catch the Ez field when it is
     # almost zero and get a distorted view.)
-    sim.run(mp.at_every(1/fcen/20, mp.output_efield_z), until=1/fcen)
+    sim.run(mp.at_every(1 / fcen / 20, mp.output_efield_z), until=1 / fcen)
+
 
 if __name__ == '__main__':
     main()
